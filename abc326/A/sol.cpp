a58b753b59c3e8bc128ec16e7fcea16a7de90292
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2023-10-29 20:39:24 +0200

    abc326 A

---
+++ abc326/A/sol.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+  int TC, x, y;
+  scanf("%d%d", &x, &y);
+  if(y-x<= 2 && y-x>=-3) printf("Yes\n");
+  else printf("No\n");
+}
